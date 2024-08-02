88168c6aa888dbafbc60abb615d6adf40cc404b6
kauane <kauane.scheidt@escola.pr.gov.br>
2024-08-02 13:49:48 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
+  createCanvas(400, 400);
+}
+
+function draw() {
+  stroke("blue");
+fill("red")
+  if(mouseIsPressed);
+  rect(mouseX, mouseY, 20, 35);
+ 
+
   }
